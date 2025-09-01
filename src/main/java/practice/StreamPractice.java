b57f5e00185744249be26003fcb9e34df7588333
anton <anton.surzhok@gmail.com>
2025-09-01 08:59:02 +0300

    Add changes

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import model.Candidate;
 import model.Person;
+import core.basesyntax.CandidateValidator;
 
 public class StreamPractice {
     /**
