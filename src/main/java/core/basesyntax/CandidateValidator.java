c57b1001c88b2d0828cbc6f396a5182a2390b5b3
anton <anton.surzhok@gmail.com>
2025-09-03 16:50:33 +0300

    Add changes

--- src/main/java/core/basesyntax/CandidateValidator.java
+++ src/main/java/core/basesyntax/CandidateValidator.java
@@ -8,7 +8,6 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
     private static final int MIN_YEARS_IN_COUNTRY = 10;
     private static final String SEPARATOR = "-";
-    git add .
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MIN_AGE
